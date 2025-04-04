714cba34a3523f67d445da95dda0064256f18ac9
DiegoAlvaradoUCA <105072745+DiegoAlvaradoUCA@users.noreply.github.com>
2025-04-03 18:24:37 -0600

    Update: Main.java

--- Laboratorio1Capas/src/Main.java
+++ Laboratorio1Capas/src/Main.java
@@ -1,4 +1,6 @@
+import controller.DoctorController;
 import controller.PatientController;
+import service.DoctorService;
 import service.PatientService;
 
 import java.util.Scanner;
@@ -8,17 +10,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         PatientService patientService = new PatientService();
         PatientController patientController = new PatientController(patientService);
+        DoctorService doctorService = new DoctorService();
+        DoctorController doctorController = new DoctorController(doctorService);
 
-        while(true){
-            System.out.println("\n Main Menu");
+        while (true) {
+            System.out.println("\n=== Main Menu ===");
             System.out.println("1. Add new patient");
             System.out.println("2. List all patients");
-            System.out.println("3. Exit");
+            System.out.println("3. Add new doctor");
+            System.out.println("4. List all doctors");
+            System.out.println("5. Exit");
 
             int option = scanner.nextInt();
             scanner.nextLine();
 
-            switch (option){
+            switch (option) {
                 case 1:
                     patientController.addPatient();
                     break;
@@ -26,9 +32,20 @@ public class Main {
                     patientService.listPatients();
                     break;
                 case 3:
+                    System.out.println("Adding new doctor...");
+                    doctorController.addDoctor();
+                    break;
+                case 4:
+                    System.out.println("Listing all doctors...");
+                    doctorController.listDoctors();
+                    break;
+
+                case 5:
+                    System.out.println("Exiting program...");
                     System.exit(0);
+                default:
+                    System.out.println("Invalid option. Please try again.");
             }
         }
-
     }
 }
