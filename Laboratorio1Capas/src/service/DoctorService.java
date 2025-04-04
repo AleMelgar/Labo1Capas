f09fbcd6d2992dd30bccf7f7a341a6a80be564c0
DiegoAlvaradoUCA <105072745+DiegoAlvaradoUCA@users.noreply.github.com>
2025-04-03 18:23:47 -0600

    Update: DoctorService.java

--- Laboratorio1Capas/src/service/DoctorService.java
+++ Laboratorio1Capas/src/service/DoctorService.java
@@ -1,4 +1,40 @@
+
 package service;
 
+import model.entity.Doctor;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DoctorService {
+    private List<Doctor> doctors;
+
+    public DoctorService() {
+        this.doctors = new ArrayList<>();
+    }
+
+    public void addDoctor(String firstName, String lastName, String dui, LocalDate birthdate, String specialty, String recruitmentDate) {
+        Doctor newDoctor = new Doctor(firstName, lastName, dui, birthdate, specialty, recruitmentDate);
+        doctors.add(newDoctor);
+        System.out.println("Doctor added successfully! Total doctors: " + doctors.size());
+    }
+
+
+    public void listDoctors() {
+        if (doctors.isEmpty()) {
+            System.out.println("No doctors registered");
+            return;
+        }
+
+        System.out.println("List of doctors:");
+        for (Doctor doctor : doctors) {
+            System.out.println("Name: " + doctor.getFirstName() + " " + doctor.getLastName() +
+                    " | DUI: " + doctor.getDui() +
+                    " | Birthdate: " + doctor.getBirthdate() +
+                    " | Specialty: " + doctor.getSpecialty() +
+                    " | Recruitment Date: " + doctor.getRecruitmentDate() +
+                    " | Epic Code: " + doctor.getEpicCode());
+        }
+    }
+
 }
